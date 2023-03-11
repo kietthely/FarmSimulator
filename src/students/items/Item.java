f2d07cb5586a6e503879209d6921d18f59973e4d
Sam Lee <lythekiet333@gmail.com>
2023-03-11 15:57:32 +1030

    Add essential features to Item class
    Added attributes, functions to the class

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,70 @@
 package students.items;
 
-public class Item {
+/**
+ * Base class for all items in the game
+ * 
+ * 
+ */
+public abstract class Item {
+	
+	private int age, deathAge, maturationAge, monetaryValue;
 
+	/**
+	 * Initialize all the item's attributes
+	 * @param maturationAge
+	 * @param deathAge
+	 * @param monetaryValue
+	 */
+	protected Item( int maturationAge, int deathAge, int monetaryValue ) {
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+		this.monetaryValue = monetaryValue;
+		this.age = 0;
+		
+	}
+	
+	public void tick() {
+		age +=1;
+	}
+	
+	public void setAge(int inputAge) {
+		age = inputAge;
+	}
+	
+	public boolean died(int currentAge) {
+		if(currentAge > deathAge) {		
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	/**
+	 * Return monetary value if it's ready to harvest otherwise returns 0
+	 * @return monetaryValue
+	 */
+	public int getValue() {
+		if (age >= maturationAge) {
+			return monetaryValue;
+		} else {
+			return 0;
+		}
+	}
+	
+	/**
+	 * See if two objects have exactly the same information
+	 * @param otherItem
+	 * @return
+	 */
+	public boolean equals(Item otherItem) {
+		if(age == otherItem.age && deathAge == otherItem.deathAge && maturationAge == otherItem.maturationAge && monetaryValue == otherItem.monetaryValue) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	
+	public abstract String toString();
 	
 }
